3f1d70ee2d93d398ea6d66d3aee518be88a5148f
matthewbeckett1 <98397750+matthewbeckett1@users.noreply.github.com>
2022-02-16 12:35:25 +0000

    Update Practice_CNN_MB.py
    Improve the data that's being inputted

--- Practice_CNN_MB.py
+++ Practice_CNN_MB.py
@@ -31,14 +31,6 @@ path_test = 'C:\\Users\\matth\\OneDrive - University of Bristol\\Documents Year 
 train = listdir(path_train)
 test = listdir(path_test)
 
-#Checking the paths work
-# A = train[:5]
-# B = test[:5]
-# print(A)
-# print()
-# print(B)
-# print(type(train))
-
 def load_train():
     train_data = []
     for img in os.listdir(path_train):
@@ -67,15 +59,20 @@ print(test_data.shape)
 train_labels = pd.read_csv('C:\\Users\\matth\\OneDrive - University of Bristol\\Documents Year 4\\Introduction to Artificial Intelligence\\Group Project\\Data\\data_labels_train.csv')['label'].tolist()
 train_labels = np.array(train_labels)
 #Given data download messed up have to change the labels size
-train_labels = train_labels[:999]
+train_labels = train_labels[:9000]
 
 test_labels = pd.read_csv('C:\\Users\\matth\\OneDrive - University of Bristol\\Documents Year 4\\Introduction to Artificial Intelligence\\Group Project\\Data\\data_labels_test.csv')['label'].tolist()
 test_labels = np.array(test_labels)
 #Given data download messed up have to change the labels size
 test_labels = test_labels[:9000]
 
-
-print(test_labels[:5])
+#Checking the paths work
+print('The first 5 images from train_data are: ', train[:5])
+print('And their labels are: ', train_labels[:5])
+print()
+print('The first 5 images from test_data are: ', test[:5])
+print('And their labels are: ', test_labels[:5])
+# #print(type(train))
 
 #So we no have train_data, train_labels, test_data and test_labels
 
@@ -106,7 +103,7 @@ def build_fit_eval_model(train_data, test_data, train_labels, test_labels):
   model.compile(loss='binary_crossentropy', optimizer=SGD(learning_rate=0.001), metrics=['accuracy'])
 
   # fit model here
-  model.fit(train_data, train_labels, epochs=10)
+  model.fit(train_data, train_labels, epochs=3)
 
   # evaluate model on test set here
   results = model.evaluate(test_data, test_labels)
